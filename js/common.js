599e3587e1a8ce1e1c6028a09e930dcc7ce5c9f4
creamchu <whdnqls95@gmail.com>
2023-07-15 22:08:28 +0900

    작업

--- js/common.js
+++ js/common.js
@@ -13,12 +13,6 @@ var fn = (function() {
 			//bottomarea
 			fn.bottomarea();
 
-			//bookmark
-			fn.bookmark();
-
-			//tab
-			fn.tab();
-
 			//select
 			fn.select();
 		},
@@ -26,30 +20,8 @@ var fn = (function() {
 		//bottomarea: function(){
 		bottomarea : function(){
 			if($(".bottom_area").length > 0) {
-				$(".content").css("padding-bottom", "80px");
+				$(".content").addClass("is_bottom_area");
 			}
-		},
-
-		//bookmark: function(){
-		bookmark : function(){
-			$(document).on("click", ".bookmark", function(){
-				if($(this).hasClass("is-active")){
-					$(this).removeClass("is-active");
-					$(this).find('span').text("즐겨찾기 해제");
-				}else{
-					$(this).addClass("is-active");
-					$(this).find('span').text("즐겨찾기");
-				}
-			});
-		},
-
-		//tab
-		tab : function(){
-			$(document).on("click", ".tab-menu a", function() {
-				var index = $(this).index();
-				$(this).addClass("active").siblings().removeClass("active");
-				$(".tab-cont > div").eq(index).addClass("active").siblings().removeClass("active");
-			});
 		},
 
 		//select
@@ -84,17 +56,22 @@ var fn = (function() {
 $(window).on('load', function(){
 	//init
 	fn.common();
-	
-	//cardswiper
-	var $cardswiperObj = $(".cardswiper");
-	if ( $cardswiperObj.find(".swiper-slide").length > 1 ){
-			var cardswiper = new Swiper(".cardswiper", {
-					slidesPerView: 1,
-					spaceBetween: 20,
-					observer: true,
-					observeParents: true,
-			});
-	}
 
+	//datepicker
+	$('.datepicker input').datepicker({
+		dateFormat: 'yy.mm.dd', //날짜 포맷이다.
+		//prevText: '이전 달',	// 마우스 오버시 이전달 텍스트
+		//nextText: '다음 달',	// 마우스 오버시 다음달 텍스트
+		closeText: '닫기', // 닫기 버튼 텍스트 변경
+		currentText: '오늘', // 오늘 텍스트 변경
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],	//한글 캘린더중 월 표시를 위한 부분
+		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],	//한글 캘린더 중 월 표시를 위한 부분
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],	//한글 캘린더 요일 표시 부분
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],	//한글 요일 표시 부분
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],	// 한글 요일 표시 부분
+		showMonthAfterYear: true,	// true : 년 월	false : 월 년 순으로 보여줌
+		yearSuffix: '년',
+		showButtonPanel: true,	// 오늘로 가는 버튼과 달력 닫기 버튼 보기 옵션
+	}).datepicker('setDate',new Date());
 });
 
